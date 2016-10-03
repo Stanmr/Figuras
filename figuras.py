4b1ab2e0627179279310d2467cbed3de1f568d9e
Stanmr <pachuquin_8@hotmail.com>
2016-10-03 11:13:54 -0500

    Se agrego archivo de configuración para Travis CI

--- figuras.py
+++ figuras.py
@@ -1,8 +1,8 @@
 class Figuras:
 
-	def cuadrado(self,lado):
-		try:
-			lado = float(lado)
-			return lado * lado
-		except Exception, e:
-			return 'dato incorrecto'
+    def cuadrado(self, lado):
+        try:
+            lado = float(lado)
+            return lado * lado
+        except Exception:
+            return 'dato incorrecto'
