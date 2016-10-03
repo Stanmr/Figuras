b41376b8f2b82b742985864e024b4c5eba8422d6
Stanmr <pachuquin_8@hotmail.com>
2016-10-03 10:25:47 -0500

    Primer commit

---
+++ figurastest.py
@@ -0,0 +1,30 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from figuras import Figuras
+
+class TestFiguras(unittest.TestCase):
+
+	def setUp(self):
+		self.figura=Figuras()
+
+	def test_area_cuadrado_lado_5(self):
+		figura = Figuras()
+		resultado = figura.cuadrado(5)
+		self.assertEqual(25, resultado)
+
+	def test_area_cuadrado_lado_6(self):
+		figura = Figuras()
+		resultado = figura.cuadrado(6)
+		self.assertEqual(36, resultado)
+
+	def test_area_cuadrado_lado_g(self):
+		figura = Figuras()
+		resultado = figura.cuadrado('g')
+		self.assertEqual('dato incorrecto', resultado)
+
+	def TeardDown(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
